e39d2eb290e440e725e335ffb6a905395ae74861
LeeJU0912 <thirdspirit@naver.com>
2023-01-31 01:29:50 +0900

    Initial Commit

---
+++ Test_iOS/Test_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_iOSApp.swift
+//  Test_iOS
+//
+//  Created by 이재욱 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct Test_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
